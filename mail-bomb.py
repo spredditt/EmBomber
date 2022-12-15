430e638bb18d8a3d30b1fa98d8ffca3701cd7625
spredditt <spredditt@proton.me>
2022-12-14 20:25:56 -0800

    done with syntaxing

--- mail-bomb.py
+++ mail-bomb.py
@@ -2,7 +2,7 @@
 
 # Author: spredditt
 
-import sys, os
+import sys, os, getpass
 
 class bcolors:
 	OKGREEN = '\033[92m'
@@ -11,7 +11,8 @@ class bcolors:
 	ENDC = '\033[0m'
 
 
-def helpmenu():
+def helpmenu(message):
+    print(message)
     try:
         doc = open('documentation.txt', 'r')
         print(' ')
@@ -20,7 +21,29 @@ def helpmenu():
     except IOError:
         print('Banner File not found')
 
-if sys.argv[1] in ('-a', '-t', '-m', '-h', '--host-mail', '--target', '--mail-type', '--help'):
-    print('Pass')
-else:
-    helpmenu()
+hostmail = ''
+hostpass = ''
+targetmail = ''
+mailtype = ''
+
+validSyntax = True
+
+try:
+    if sys.argv[1] in ('-h', '--help'):
+        helpmenu('')
+
+    else:
+        for i in range(1, 6, 2):
+            if sys.argv[i] not in ('-a', '-t', '-m', '--host-mail', '--target-mail', '--mail-type'):
+                helpmenu('\nInvalid syntax')
+                SystemExit
+            else:
+            
+                if sys.argv[i + 1] == '' or sys.argv[i + 1] in ('-a', '-t', '-m', '--host-mail', '--target-mail', '--mail-type'):
+                    helpmenu(f'\nInvalid value at {sys.argv[i]}')
+                    break
+                else:
+                    hostpass = getpass.getpass('host-password: ')
+                    SystemExit
+except IndexError:
+    helpmenu('')
